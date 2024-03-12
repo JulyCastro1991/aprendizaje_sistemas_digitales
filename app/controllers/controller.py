bdc664740f9a7cfb2efaa1b9b5787fcd0da866e8
JulyCastro1991 <96405167+JulyCastro1991@users.noreply.github.com>
2024-03-12 00:48:34 -0500

    updates

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -9,7 +9,7 @@ import requests
 
 # puerto_serie = serial.Serial('COM3', 115200, timeout=1)
 
-ip_servidor = 'http://192.168.43.190'
+ip_servidor = 'http://192.168.137.219'
 
 @app.route('/')
 def index():
@@ -22,13 +22,22 @@ def operaciones_binarias():
         term2 = request.form['term2']
         operador = request.form['operador']
         salida = calculadora_binaria(term1, term2, operador)
-        resp = requests.post(f'{ip_servidor}/operaciones_binarias', data=salida)
-
-        # puerto_serie.write(str(salida).encode())
 
         return salida
 
     return render_template('operaciones_binarias.html')
+
+@app.route('/operaciones_binarias_esp', methods=['POST'])
+def operaciones_binarias_esp():
+    if request.method == 'POST':
+        term1 = request.form['term1']
+        term2 = request.form['term2']
+        operador = request.form['operador']
+        salida = calculadora_binaria(term1, term2, operador)
+        resp = requests.post(f'{ip_servidor}/operaciones_binarias', data=salida)
+
+        return salida
+
 
 @app.route('/conversor', methods=['GET', 'POST'])
 def conversor():
@@ -55,20 +64,31 @@ def operaciones_logicas():
         D_val = request.form['B_val']
 
         salida = calculadora_operaciones_logicas(expression, A_val,  B_val, C_val, D_val)
-        # puerto_serie.write((str(salida['valor']) + "\n" + "Simplificada: "+ "\n" + str(salida['simplificada'])).encode())
-        data = (str(salida['valor']) + "\n" + "Funcion: "+ "\n" + str(salida['simplificada'])).encode() 
-        resp = requests.post(f'{ip_servidor}/operaciones_logicas', data=data)
 
         return  salida
-    
+        
     return render_template('operaciones_logicas.html')
 
+@app.route('/operaciones_logicas_esp', methods=['POST'])
+def operaciones_logicas_esp():
+    if request.method == 'POST':
+        expression = request.form['expression']
+        A_val = request.form['A_val']
+        B_val = request.form['B_val']
+        C_val = request.form['C_val']
+        D_val = request.form['B_val']
+
+        salida = calculadora_operaciones_logicas(expression, A_val,  B_val, C_val, D_val)
+        data = (str(salida['valor']) + "\n" + "Funcion: "+ "\n" + str(salida['simplificada'])).encode() 
+        requests.post(f'{ip_servidor}/operaciones_logicas', data=data)
+
+        return  salida
 
 @app.route('/mostrar_tabla', methods=['POST'])
 def mostrar_tabla():
     if request.method == 'POST':
         data = request.args.get("contador", "")
-
+        
         print(data)
         requests.post(f'{ip_servidor}/mostrar_tabla', data=data)
 
